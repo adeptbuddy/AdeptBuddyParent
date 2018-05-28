dba200b55e8456aa2ca19031714956790b2332c7
Krishna Chaitanya <kchait11@ms.ds.uhc.com>
2018-05-28 17:12:41 +0530

    Initial Changes

--- app/component.app.ts
+++ app/component.app.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app',
-    template: '<h1>Adept Buddy</h1>'
+    templateUrl: './partials/app.html'
 })
 
-export class AppComponent {}
+export class AppComponent {
+    name: string;
+
+    constructor(){
+        this.name = "Krishna Chaitanya K";
+    }
+}
